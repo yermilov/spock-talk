3ac345024609d4bd762b2db59f22d5e2bcb6a934
Yaroslav Yermilov <yermilov17@gmail.com>
2016-04-30 13:17:56 +0300

    system properties

---
+++ src/test/groovy/N31S_SystemProperties.groovy
@@ -0,0 +1,28 @@
+import spock.lang.*
+import spock.util.environment.RestoreSystemProperties
+
+@Title('Spock system properties')
+@Narrative('''
+As JEEConf speaker
+I want to show that Spock is careful with system properties
+''')
+@Stepwise
+class N31S_SystemProperties extends Specification {
+
+    @RestoreSystemProperties
+    def 'set spock version'() {
+        expect: 'that spock.version is not set'
+        System.getProperty('spock.version') == null
+
+        when: 'spock version is set'
+        System.setProperty('spock.version', '1.0')
+
+        then: 'we can retrieve its value back'
+        System.getProperty('spock.version') == '1.0'
+    }
+
+    def 'check spock version is not set'() {
+        expect: 'that spock.version is not set'
+        System.getProperty('spock.version') == null
+    }
+}
