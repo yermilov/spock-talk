479b08cd3bed7945b041531920b4eef18a17fbfd
Yaroslav Yermilov <yermilov17@gmail.com>
2016-05-02 16:54:39 +0300

    time travel

---
+++ src/test/java/N35T_Timeout.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
+public class N35T_Timeout {
+
+    @Test(timeOut = 2000)
+    public void infiniteLoop() {
+        // setup
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        // run
+        while (true) { arrayList.add("testng forever!"); }
+    }
+}
